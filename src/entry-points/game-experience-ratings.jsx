d98a8ace220cb19f792b4def5efd5c9795d8216e
Sethu Nguna <64438772+Seth-arc@users.noreply.github.com>
2024-11-21 03:51:33 +0000

    yebo

--- src/entry-points/game-experience-ratings.jsx
+++ src/entry-points/game-experience-ratings.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import GameExperienceRatings from '../components/GameExperienceRatings';
-import '/src/styles/tailwind.css';
+import '../styles/tailwind.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import '/src/styles/tailwind.css';
+import './index.css';
 
 const App = () => {
   return (
